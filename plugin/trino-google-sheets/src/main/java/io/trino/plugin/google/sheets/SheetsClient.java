60b1a18a9732152ed2336e659c18b0441295e800
Evgenii Golubaev <golubaev_e@flowwow.com>
2025-01-27 14:14:32 +0400

    DTPL-1089 | Added TRUNCATE for Google Sheets connector

--- plugin/trino-google-sheets/src/main/java/io/trino/plugin/google/sheets/SheetsClient.java
+++ plugin/trino-google-sheets/src/main/java/io/trino/plugin/google/sheets/SheetsClient.java
@@ -245,7 +245,7 @@ public class SheetsClient
 
     public void clearSheet(String sheetExpression)
     {
-        SheetsSheetIdAndRange sheetIdAndRange = new SheetsSheetIdAndRange(sheetExpression + "!2:9999999");
+        SheetsSheetIdAndRange sheetIdAndRange = new SheetsSheetIdAndRange(sheetExpression);
         ClearValuesRequest clearValuesRequest = new ClearValuesRequest();
 
         try {
