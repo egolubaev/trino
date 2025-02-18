f144bec27ee6b8e789f3c4907f12291dfb0df565
Evgenii Golubaev <golubaev_e@flowwow.com>
2025-02-18 18:42:49 +0400

    DTPL-1089 | Excluded header row from Google Sheets TRUNCATE operation

--- plugin/trino-google-sheets/src/main/java/io/trino/plugin/google/sheets/SheetsClient.java
+++ plugin/trino-google-sheets/src/main/java/io/trino/plugin/google/sheets/SheetsClient.java
@@ -238,7 +238,7 @@ public class SheetsClient
 
     public void clearSheet(String sheetExpression)
     {
-        SheetsSheetIdAndRange sheetIdAndRange = new SheetsSheetIdAndRange(sheetExpression);
+        SheetsSheetIdAndRange sheetIdAndRange = new SheetsSheetIdAndRange(sheetExpression + "!2:9999999");
         ClearValuesRequest clearValuesRequest = new ClearValuesRequest();
 
         try {
